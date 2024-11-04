3ac5c1cc47d47abd2b76fe308239f5948bf9a6e9
jiseom <76646494+jiseom@users.noreply.github.com>
2024-11-05 01:59:17 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        //StringBuilder sb = new StringBuilder();
+        
+        // for (char c : myString.toCharArray()){
+        //     if(c >= 97){
+        //         sb.append(c);
+        //     }else{
+        //         sb.append(String.valueOf(c).toLowerCase());   
+        //     }
+        // }
+        //return sb.toString();
+        return myString.toLowerCase();
+
+    }
+}
