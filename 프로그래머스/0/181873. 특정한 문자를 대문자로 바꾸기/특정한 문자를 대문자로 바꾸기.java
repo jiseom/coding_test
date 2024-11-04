23d5d2a7cfa57c3a52c867e2d53508aa87a8fa4c
jiseom <76646494+jiseom@users.noreply.github.com>
2024-11-05 00:41:13 +0900

    특정한 문자를 대문자로 바꾸기

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -1,6 +1,18 @@
 class Solution {
     public String solution(String my_string, String alp) {
         String answer = "";
+        char a = alp.charAt(0) ;
+
+        for (char c : my_string.toCharArray()){
+            if(a == c){
+                int b = (int) a - 32 ;
+                answer += (char)(b);
+
+            }else{
+                answer += c;
+            }
+        }
+
         return answer;
     }
 }
