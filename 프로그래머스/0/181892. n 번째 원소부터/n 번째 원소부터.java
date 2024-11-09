95d0dbf5f7cc0d321dd2361205c884d282d1dc6f
jiseom <76646494+jiseom@users.noreply.github.com>
2024-11-10 00:22:27 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length - n + 1];
+        int idx = n-1;
+        
+        for(int i = 0 ; i < num_list.length - n + 1; i++ ){
+            answer[i] = num_list[idx++];    
+        }
+        
+        return answer;
+    }
+}
