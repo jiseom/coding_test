24f633fa70b7145573ef470f6d0b2f1cf42219df
jiseom <76646494+jiseom@users.noreply.github.com>
2025-07-27 16:53:45 +0900

    [Silver V] Title: 나이순 정렬, Time: 1012 ms, Memory: 101480 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,27 +1,29 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         String[][] arr = new String[number][2];
-
-
         for (int i = 0; i < number; i++) {
             arr[i][0] = scanner.next();
             arr[i][1] = scanner.next();
         }
 
-        Arrays.sort(arr, new Comparator<String[]>() {
+        Arrays.sort(arr, new Comparator<String[]>() { 
             @Override
             public int compare(String[] o1, String[] o2) {
                 return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
             }
+
         });
 
-        for (int i = 0; i < number; i++) {
-            System.out.println(arr[i][0] + " " + arr[i][1]);
-        }
-    }
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = 0; i < number; i++) {
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append("\n");
+        }
+        
+        System.out.print(sb);
+
+    }
 }
