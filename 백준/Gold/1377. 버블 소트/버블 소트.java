329555d7b282c07bc0fe495d80a56c1b795e5494
jiseom <76646494+jiseom@users.noreply.github.com>
2025-07-28 18:46:07 +0900

    [Gold II] Title: 버블 소트, Time: 332 ms, Memory: 47860 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -1,42 +1,29 @@
-import java.util.*;
 import java.io.*;
 public class Main {
-
-    static class Element{
-        int value;
-        int originalIndex;
-
-        Element(int value,int idx){
-            this.value = value;
-            this.originalIndex = idx;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
-
-        List<Element> list = new ArrayList<>();
+        int[] input = new int[N];
+        int[] count = new int[1_000_001];
 
         for (int i = 0; i < N; i++) {
-            int value = Integer.parseInt(bf.readLine());
-            Element em = new Element(value, i);
-            list.add(em);
+            input[i] = Integer.parseInt(bf.readLine());
+            count[input[i]]++;
         }
-
-        list.sort((a,b) -> {
-            return a.value - b.value;
-        });
-
-        int max = 0;
-        for (int i = 0; i <= list.size()-1; i++) {
-            Element e = list.get(i);
-            int move =  e.originalIndex - i;
-            if (max < move) {
-                max = move;
-            }
+        
+        int accumulated = 0; 
+        for (int i = 0; i <= 1_000_000; i++) {
+            accumulated += count[i];
+            count[i] = accumulated- 1; 
         }
-
-       System.out.print(max + 1);
+        
+        int maxMove = 0;
+        for (int i = 0; i < N; i++) {
+            int sortedIndex = count[input[i]];
+            maxMove = Math.max(maxMove,i-sortedIndex);
+        }
+        
+        System.out.println(maxMove+1);
     }
+    
 }
