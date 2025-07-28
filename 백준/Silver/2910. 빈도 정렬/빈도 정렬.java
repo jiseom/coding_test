7fedf83336aa12538b3aa604b52cde913c962a34
jiseom <76646494+jiseom@users.noreply.github.com>
2025-07-28 15:15:01 +0900

    [Silver III] Title: 빈도 정렬, Time: 116 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,44 +1,58 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
+    static class NumberStat implements Comparable<NumberStat> {
+
+        int value;
+        int count;
+        int order;
+
+        @Override
+        public int compareTo(NumberStat n) {
+            if (this.count == n.count) {
+                return this.order - n.order;
+            } else {
+                return n.count - this.count;
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) throws IOException {
-        List<Integer> list = new ArrayList<>();
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
+        String[] arr = bf.readLine().split(" ");
+        int N = Integer.parseInt(arr[0]);
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer,NumberStat> map = new HashMap<>();
+        List<NumberStat> list= new ArrayList<>();
 
-        StringTokenizer tokenizer = new StringTokenizer(bf.readLine(), " ");
+        String[] arr2= bf.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            int number = Integer.parseInt(tokenizer.nextToken());
-            map.put(number, map.getOrDefault(number, 0) + 1);
-
-            if (!list.contains(number)) {
-                list.add(number);
+            int number = Integer.parseInt(arr2[i]);
+            if (!map.containsKey(number)) {
+                NumberStat stat = new NumberStat();
+                stat.value = number;
+                stat.count = 0;
+                stat.order = i;
+                map.put(number, stat);
+                list.add(stat);
             }
+            map.get(number).count++;
         }
 
-        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
-        entryList.sort((a, b) ->
-        {
-            int compare = b.getValue() - a.getValue();
-            if (compare != 0) return compare;
-            return list.indexOf(a.getKey()) - list.indexOf(b.getKey());
-        });
-
-
+        Collections.sort(list);
+        
         StringBuilder sb = new StringBuilder();
-        for (Map.Entry<Integer, Integer> entry : entryList) {
-            int valueNumber = entry.getKey();
-            int valueCount = entry.getValue();
-
-            for (int i = 0; i < valueCount; i++) {
-                sb.append(valueNumber).append(" ");
+        for (NumberStat stat : list){
+            while (stat.count > 0) {
+                sb.append(stat.value).append(" ");
+                stat.count--;
             }
-
         }
+
         System.out.print(sb);
+
     }
 }
