b2dfd484d91145d685e061a25c462a49698bec12
jiseom <76646494+jiseom@users.noreply.github.com>
2025-08-11 22:34:15 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.12 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 78.4 MB, 시간: 13.87 ms
+메모리: 79.7 MB, 시간: 9.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 11일 18:47:35
+2025년 08월 11일 22:34:13
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,66 +1,13 @@
 import java.util.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String [] strArr1 = new String[n];
-        String [] strArr2 = new String[n];
-        
-        int temp = 0;
-        int binary = 0;
-       
+   String[] answer = new String[n];
         for(int i= 0; i<n; i++){
-            temp = arr1[i];
-            String s = "";
-            
-            while (temp > 0){
-                binary = temp % 2;
-                temp /= 2;
-                s = binary + s;
-            }
-            
-           while (s.length() < n) s = "0" + s;
-            
-            strArr1[i] = s;
-        }
-         
-        for(int i= 0; i<n; i++){
-            temp = arr2[i];
-            String s = "";
-            
-            while (temp > 0){
-                binary = temp % 2;
-                temp /= 2;
-                s = binary + s;
-            }
-            
-            // if(s.length() < n){
-            //     s = "0"+ s;
-            // }
-            while (s.length() < n) s = "0" + s;
-            strArr2[i] = s;
-        }
-        
-        //System.out.println(Arrays.toString(strArr1));
-        //System.out.println(Arrays.toString(strArr2));
-        
-        for(int i= 0; i < n; i++){
-            char[] a = strArr1[i].toCharArray();
-            char[] b = strArr2[i].toCharArray();
-            for (int j =0; j<n; j++){    
-                
-                if(a[j] == '0' && b[j] == '1'){
-                    a[j] = '1';
-                }
-            }
-            strArr1[i] = new String(a);
-            
-        }
-        
-    
-        String[] answer = new String[n];
-        for (int i = 0; i < n; i++) {
-            answer[i] = strArr1[i].replace('1', '#').replace('0', ' ');
+            String str = Integer.toBinaryString(arr1[i]|arr2[i]);
+            answer[i] = String.format("%" + n + "s", str).replace("1","#").replace("0"," ");
+//            answer[i] = str.replace("1","#").replace("0"," ");
+
         }
         return answer;
-
     }
 }
